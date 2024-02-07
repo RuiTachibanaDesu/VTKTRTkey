66bb9a948d98b22c860367ae0e0cb1c45123e74c
RuiTachibanaDesu <alisedenk689@gmail.com>
2024-02-08 02:50:11 +0700

    KeybuatVTK

--- devicesadd.sh
+++ devicesadd.sh
@@ -4,23 +4,20 @@
 local_file="/data/user/0/com.termux/files/home/VTKTRTkey/device_ids.txt"
 
 # Token akses GitHub untuk otentikasi
-access_token="your_personal_access_token_here"
+access_token="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"
 
 # Nama branch
 branch="main"
 
-# Mengatur nama pengguna Git secara global
-git config --global user.name "RuiTachibanaDesu"
-
-# Mengatur alamat email Git secara global
-git config --global user.email "alisedenk689@gmail.com"
+# Nama pengguna Git
+git_username="YourGitUsername"
 
 # Menambahkan perubahan ke staging area
 git add .
 
 # Melakukan commit perubahan dengan nama pengguna
-git commit -m "Add existing file"
+git commit -m "Add existing file" --author="$git_username"
 
-# Mendorong perubahan ke GitHub dengan token akses
-git push origin "$branch" --quiet --set-upstream "origin" "$branch"
+# Mendorong perubahan ke GitHub
+git push origin "$branch"
 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
